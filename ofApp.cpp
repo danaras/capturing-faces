54e9439e497a17740e87664e3229f2ed60df2ca6
Danara Sarioglu <ds4489@nyu.edu>
2016-04-11 00:47:12 -0400

    allignment and averaging added

--- ofApp.cpp
+++ ofApp.cpp
@@ -1,177 +1,175 @@
-// =============================================================================
-//
-// IP camera video grabber: Copyright (c) 2009-2016 Christopher Baker <http://christopherbaker.net>
-//
-// =============================================================================
-
-
 #include "ofApp.h"
 using namespace ofxCv;
 
 
 void ofApp::setup()
 {
+    sender.setup(HOST, PORT);
     ofSetLogLevel(OF_LOG_VERBOSE);
     ofSetFrameRate(30);
-    loadCameras();
-    
-    // initialize connection
-    for (std::size_t i = 0; i < NUM_CAMERAS; i++)
-    {
-        IPCameraDef& cam = getNextCamera();
-
-        std::shared_ptr<Video::IPVideoGrabber> c = std::make_shared<Video::IPVideoGrabber>();
-
-        // if your camera uses standard web-based authentication, use this
-        // c->setUsername(cam.username);
-        // c->setPassword(cam.password);
-        
-        // if your camera uses cookies for authentication, use something like this:
-        // c->setCookie("user", cam.username);
-        // c->setCookie("password", cam.password);
-        
-        c->setCameraName(cam.getName());
-        c->setURI(cam.getURL());
-        c->connect(); // connect immediately
-
-        // if desired, set up a video resize listener
-        ofAddListener(c->videoResized, this, &ofApp::videoResized);
-        
-        grabbers.push_back(c);
-
-    }
+    ofLog(OF_LOG_NOTICE, "---------------Loading Stream---------------");
+    IPCameraDef cam("http://" + ip + "/axis-cgi/mjpg/video.cgi");
+    std::shared_ptr<Video::IPVideoGrabber> c = std::make_shared<Video::IPVideoGrabber>();
+    c->setCameraName(cam.getName());
+    c->setURI(cam.getURL());
+    c->connect(); // connect immediately
+    grabber = c;
     tracker.setup();
     tracker.setTolerance(0.1);
+    fbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGB);
+    pixelSum.allocate(400, 500, OF_IMAGE_COLOR);
+    pixelAverage.allocate(400, 500, OF_IMAGE_COLOR);
+    faceVec.resize(60000);
+
 }
-
-
-IPCameraDef& ofApp::getNextCamera()
-{
-    nextCamera = (nextCamera + 1) % ipcams.size();
-    return ipcams[nextCamera];
-}
-
-
-void ofApp::loadCameras()
-{
-    
-    // all of these cameras were found using this google query
-    // http://www.google.com/search?q=inurl%3A%22axis-cgi%2Fmjpg%22
-    // some of the cameras below may no longer be valid.
-    
-    // to define a camera with a username / password
-    //ipcams.push_back(IPCameraDef("http://148.61.142.228/axis-cgi/mjpg/video.cgi", "username", "password"));
-
-    ofLog(OF_LOG_NOTICE, "---------------Loading Streams---------------");
-    IPCameraDef def("http://ip/axis-cgi/mjpg/video.cgi");
-
-
-            std::string logMessage = "STREAM LOADED: " + def.getName() +
-			" url: " +  def.getURL() +
-			" username: " + def.getUsername() +
-			" password: " + def.getPassword();
-            
-            ofLogNotice() << logMessage;
-            
-            ipcams.push_back(def);
-  
-    ofLog(OF_LOG_NOTICE, "-----------Loading Streams Complete----------");
-    
-    nextCamera = ipcams.size();
-}
-
-
-void ofApp::videoResized(const void* sender, ofResizeEventArgs& arg)
-{
-    // find the camera that sent the resize event changed
-    for (std::size_t i = 0; i < NUM_CAMERAS; ++i)
-    {
-        if (sender == &grabbers[i])
-        {
-            std::stringstream ss;
-            ss << "videoResized: ";
-            ss << "Camera connected to: " << grabbers[i]->getURI() + " ";
-            ss << "New DIM = " << arg.width << "/" << arg.height;
-            ofLogVerbose("ofApp") << ss.str();
-        }
-    }
-}
-
-
 void ofApp::update()
 {
     // update the cameras
-    for (std::size_t i = 0; i < grabbers.size(); ++i)
-    {
-        grabbers[i]->update();
-        if(grabbers[i] -> isFrameNew()) {
-            tracker.update(toCv(grabbers[i] -> getPixels()));//convert ofPixels into a cv:Mat
-        }
+    
+    grabber->update();
+    if(grabber -> isFrameNew()) {
+        tracker.update(toCv(grabber -> getPixels()));//convert ofPixels into a cv:Mat
     }
+    
 }
-
-
 void ofApp::draw()
 {
     ofBackground(0,0,0);
-
+    
     ofSetHexColor(0xffffff);
-    
-    
-    
-    
-    int w = ofGetWidth();
-    int h = ofGetHeight();
-
-    grabbers[0] -> draw(0,0,w,h);
-    
+    grabber -> draw(0,0,w,h);
     ofSetColor(255);
     ofSetLineWidth(2);
     tracker.draw();
     ofDrawBitmapString(ofToString(tracker.getDirection()), 10, 20);
-    
-    if(tracker.getFound()) {
-       
-
-        
-        if(tracker.getFound() != same && tracker.getDirection() == 0){
-        
-        
-        string fileName = pre + ofToString(counter) + ".png";
-        //tracker.draw();
- //       ofLog() << "Size: " << tracker.size() << endl;
-        ofRectangle rect = tracker.getHaarRectangle();
-        //img.grabScreen(rect.getMinX(),rect.getMinY(),rect.getWidth(),rect.getHeight());
-        img.setFromPixels(grabbers[0] -> getPixels(), w, h, OF_IMAGE_COLOR);
-        img.crop(rect.getMinX(),rect.getMinY(),rect.getWidth(),rect.getHeight());
-        img.save(fileName);
-        counter++;
-            
-        same = !same;
-        
-       
-
-        }
-    }else{
-        if(tracker.getFound() != same){
-            same = !same;
+     ofDrawBitmapString(ofToString(same), 20, 20);
+    if(tracker.getFound()){
+        rect = tracker.getHaarRectangle();
+        int x = rect.getCenter().x;
+        int y = rect.getCenter().y;
+        int z = 50*ofGetHeight()/rect.getHeight();
+        //osc
+        ofxOscMessage m;
+        m.setAddress(string(MESSAGE));
+        m.addFloatArg((float)x);
+        m.addFloatArg((float)y);
+        sender.sendMessage(m, false);
+        currentTimeZoomOut = ofGetElapsedTimeMillis();
+        differenceZoomOut = currentTimeZoomOut - startTimeZoomOut;
+        if(zoomedIn == false && same == false && differenceZoomOut > 4000){
+            startTimeZoomIn = ofGetElapsedTimeMillis();
+            ofHttpResponse zoomIn = ofLoadURL("http://" + ip + "/axis-cgi/com/ptz.cgi?areazoom=" + ofToString(x) +","+ ofToString(y) + "," + ofToString(z));
+            cout << zoomIn.data << endl;
+            zoomedIn = true;
         }
     }
-   // ofLog() << "Height:" <<  grabbers[0]->getHeight() << ", Width:" << grabbers[0]->getWidth() << endl;
+    same = true;
+    if(tracker.getFound() == false){
+        same = false;
+    }
+    if(zoomedIn == true){
+        currentTimeZoomIn = ofGetElapsedTimeMillis();
+        differenceZoomIn = currentTimeZoomIn - startTimeZoomIn;
+        if(differenceZoomIn >8000){
+            tracker.update(toCv(grabber -> getPixels()));
+            if(tracker.getFound()){
+                ofLog() << "here" << endl;
+                //cropping face according to the eyes (rotation included)
+                ofVec2f leftEye = tracker.getImageFeature(ofxFaceTracker::LEFT_EYE).getCentroid2D();
+                ofVec2f rightEye = tracker.getImageFeature(ofxFaceTracker::RIGHT_EYE).getCentroid2D();
+                
+                float rotation = (rightEye - leftEye).angle(ofVec2f(1, 0));
+                
+                float eyeDistance = leftEye.distance(rightEye);
+                float scale = eyeDistanceConstant / eyeDistance;
+
+                fbo.begin();
+                ofClear(255,255);
+                ofPushMatrix();
+
+                ofTranslate(leftEye.x,leftEye.y);
+              
+                ofRotate(rotation);
+
+                ofTranslate(-leftEye.x,-leftEye.y);
+
+                img.setFromPixels(grabber -> getPixels(), w, h, OF_IMAGE_COLOR);
+
+                img.setAnchorPoint(leftEye.x, leftEye.y);
+                
 
 
+                //img.crop(rect.getMinX(),rect.getMinY(),rect.getWidth(),rect.getHeight());
+                ofTranslate(ofGetWidth()/2,ofGetHeight()/2);
+                ofScale(scale, scale);
+                
+              
+                img.draw(0,0);
+
+                ofDrawCircle(leftEye.x, leftEye.y, 10);
+                
+                ofPopMatrix();
+                ofDrawLine(ofGetWidth()/2, 0, ofGetWidth()/2, ofGetHeight());
+                ofDrawLine(0, ofGetHeight()/2, ofGetWidth(), ofGetHeight()/2);
+              
+                
+                fbo.end();
+                fbo.readToPixels(pix);
+
+                
+                pix.crop(ofGetWidth()/2 - 2 * eyeDistanceConstant, ofGetHeight()/2 - 2 * eyeDistanceConstant, 4 * eyeDistanceConstant, 5 * eyeDistanceConstant);
+                
+                ofColor c = ofColor(pix[0], pix[1],pix[2]);
+                
+                cout << int(c.r) << endl;
+                cout << int(c.g) << endl;
+                cout << int(c.b) << endl;
+
+                numberOfFaces += 1;
+                
+                int i = 0;
+                for(int y = 0; y < 5 * eyeDistanceConstant; y++){
+                    for(int x = 0; x < 4 * eyeDistanceConstant; x++){
+                        pixelSum[i] += int(pix[i]);
+                        pixelSum[i + 1] += int(pix[i + 1]);
+                        pixelSum[i + 2] += int(pix[i + 2]);
+                        
+                        pixelAverage[i] = int(pixelSum[i]/numberOfFaces);
+                        pixelAverage[i + 1] = int(pixelSum[i + 1]/numberOfFaces);
+                        pixelAverage[i + 2] = int(pixelSum[i + 2]/numberOfFaces);
+                        
+                        i += 3;
+
+                    }
+                }
+                
+                pixelAverage.setImageType(OF_IMAGE_COLOR);
+                
+                cv::imshow("Display frame", toCv(pixelAverage));
+                
+                time_t t = time(0);   // get time now
+                struct tm * now = localtime( & t );
+                string fileName =  "face_" + ofToString(now->tm_year + 1900) + "-" + ofToString(now->tm_mon + 1) + "-" + ofToString(now->tm_mday) + "_" + ofToString(now->tm_hour) + "-" + ofToString(now->tm_min) + "-" +
+                ofToString(now->tm_sec) + ".png";
+                
+                ofSaveImage(pixelAverage,"alligned/"+fileName);
+
+            }
+            ofHttpResponse zoomOut = ofLoadURL("http://" + ip + "/axis-cgi/com/ptz.cgi?move=home");
+            cout << zoomOut.data << endl;
+            zoomedIn = false;
+            startTimeZoomOut = ofGetElapsedTimeMillis();
+        }
+    }
 }
+
 void ofApp::keyPressed(int key) {
     if(key == 'r') {
         tracker.reset();
     }
+    if(key == 'h'){
+        ofHttpResponse moveHome = ofLoadURL("http://" + ip + "/axis-cgi/com/ptz.cgi?move=home");
+        cout << moveHome.data << endl;
+        
+    }
 }
-
-//void zoom(){
-//    ofHttpResponse resp = ofLoadURL("http://ip/axis-cgi/com/ptz.cgi?areazoom=" + 100,100,800);
-//    cout << resp.data << endl;
-//    
-//    //http://<servername>/axiscgi/com/ptz.cgi?<parameter>=<value>[&<parameter>=<value>...]
-//}
-
-
